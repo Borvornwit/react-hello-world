29ffe63053400c3d19d01b6baea7e2ecd7dcfb66
Borvornwit <petebest47@gmail.com>
2021-02-04 20:54:14 +0700

    binding event handling

---
+++ src/components/EventBind.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             massage: 'Hello'
+        }
+
+        this.clickHandler = this.clickHandler.bind(this)
+    }
+
+    clickHandler() {
+        this.setState({ 
+            massage: 'Goodbye' 
+        })
+    }    
+
+    render() {
+        return (
+            <div>
+                <div>{this.state.massage}</div>
+                {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+                {/* <button onClick={() =>  this.clickHandler()}>Click</button> */}
+                <button onClick={() => this.clickHandler()}>Click</button>
+            </div>
+        )
+    }
+}
+
+export default EventBind
